3f97e199f960d9aadc2167c77c3abed8f5723fbf
Oleg Rogov <oleg@heeeybro.com>
2018-04-21 15:27:17 +0300

    Add connect to event handlers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 require('./config/config');
 
 const path = require('path'),
+	http = require('http'),
 	publicPath = path.join(__dirname, '../public'),
 	express = require('express'),
+	socketIO = require('socket.io'),
 	app = express(),
-	port = process.env.PORT;
+	port = process.env.PORT,
+	server = http.createServer(app),
+	io = socketIO(server);
 
 
 let useByApp = [
@@ -17,6 +21,10 @@ useByApp.map(result => {
 	}	
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('New user connected');
+});
+
+server.listen(port, () => {
 	console.log(`Started up at port ${port}`);
 });
