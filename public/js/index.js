84d9dbd3238fcc2620856216eabc0105f3413081
Oleg Rogov <oleg@heeeybro.com>
2018-04-22 14:48:29 +0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,3 @@
-$('body').css({
-	background: 'beige'
-});
-
 let socket = io();
 
 socket.on('connect', () => {
@@ -29,13 +25,14 @@ socket.on('newLocationMessage', (message) => {
 $('#message-form').on('submit', (e) => {
 	e.preventDefault();
 
-	let text = $('[name=message]').val();
+	let messageTextbox = $('[name=message]'),
+		text = messageTextbox.val();
 
 	socket.emit('createMessage', {
 		from: 'User',
 		text
 	}, () => {
-		$('[name=message]').val('');
+		messageTextbox.val('');
 		console.log('Message sent!')
 	});
 });
@@ -45,14 +42,19 @@ let locationButton = $('#send-location');
 locationButton.on('click', () => {
 	if (!navigator.geolocation) {
 		console.log('Geolocation not supported by your browser!');
-	}	
+	}
+
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
 
 	navigator.geolocation.getCurrentPosition((position) => {
+		locationButton.removeAttr('disabled').text('Send location');
+		
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
-		});		
+		});
 	}, () => {
+		locationButton.removeAttr('disabled').text('Send location');
 		console.log('Unable to fetch location.');
 	});
 });
